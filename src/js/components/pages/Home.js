f709ec00bb06d0613b637eddf6b695d462d895f3
Anna Lotko <annaklotko@gmail.com>
2017-07-04 07:35:46 -0600

    added boilerplate files for react

---
+++ src/js/components/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+import '../../../sass/components/pages/home.sass'
+
+export default class Home extends React.Component {
+
+  render() {
+
+    return (
+      <div>
+        <div class="home-wrap">
+          Home
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/js/components/shared/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import '../../../sass/components/shared/header.sass'
+
+export default class Header extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div class="header-wrap">
+
+      </div>
+    )
+  }
+}

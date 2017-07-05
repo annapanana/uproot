2707ca645480e4d0ac18517b400021c4995ced96
Anna Lotko <annaklotko@gmail.com>
2017-07-04 20:11:44 -0600

    fixed layout to be plant-based and updated plant icons

--- src/js/components/shared/gridElements/bed.js
+++ src/js/components/shared/gridElements/bed.js
@@ -5,13 +5,47 @@ import SubGrid from './subGrid.js';
 export default class Bed extends React.Component {
   constructor(props) {
     super(props);
+
+    this.plants = [
+      {
+        name: "broccoli",
+        image: "https://s3-us-west-2.amazonaws.com/uproot/broccoli.svg",
+        area: 1
+      },
+      {
+        name: "corn",
+        image: "https://s3-us-west-2.amazonaws.com/uproot/corn.svg",
+        area: 1
+      },
+      {
+        name: "peas",
+        image: "https://s3-us-west-2.amazonaws.com/uproot/peas.svg",
+        area: 3
+      },
+      {
+        name: "tomato",
+        image: "https://s3-us-west-2.amazonaws.com/uproot/tomato.svg",
+        area: 1
+      },
+      {
+        name: "lettuce",
+        image: "https://s3-us-west-2.amazonaws.com/uproot/lettuce.svg",
+        area: 2
+      }
+    ]
   }
-  
+
+  getRandomPlant() {
+    return this.plants[Math.floor(Math.random() * this.plants.length)];
+  }
+
   generateRow(xVal, yVal) {
+    //TODO: Get Vegetable from DB for this row ID
+    let plant = this.getRandomPlant();
     return (
       <g>
         <rect class="cell" x={xVal+this.props.xVal+5} y={yVal+this.props.yVal+5} width="100" height="100" />
-        <SubGrid dimension={2} xVal={xVal+this.props.xVal+5} yVal={yVal+this.props.yVal+5}/>
+        <SubGrid xVal={xVal+this.props.xVal+5} yVal={yVal+this.props.yVal+5} plant={plant}/>
       </g>
     )
   }
